7acee31f8c2164fa2cd747ebef877365c9f2b20e
aleksaivanic <ivanicaleksa98@gmail.com>
2021-01-30 17:36:14 +0100

    Implemented pharmacy profile frontend for pharmacy admin

--- client/angular-pharmacy/src/app/pharmacy-administrator/pharmacy-administrator-my-pharmacy/pharmacy-administrator-my-pharmacy.component.ts
+++ client/angular-pharmacy/src/app/pharmacy-administrator/pharmacy-administrator-my-pharmacy/pharmacy-administrator-my-pharmacy.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Medicine } from '../../models/medicine.model';
+import { Pharmacy } from '../../models/pharmacy.model';
+import { PharmacyService } from '../../services/pharmacy/pharmacy.service';
+import { AuthenticationService } from '../../services/users/authentication.service';
 
 @Component({
   selector: 'app-pharmacy-administrator-my-pharmacy',
@@ -6,8 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pharmacy-administrator-my-pharmacy.component.scss']
 })
 export class PharmacyAdministratorMyPharmacyComponent implements OnInit {
+  public medicineList: Medicine[] = [{code: '123', name: 'Brufen'},
+                                     {code: '321', name: 'Nimulid'}];
 
-  constructor() { }
+  public pharmacy: Pharmacy;
+  public id: number;
+  public name: string;
+  public address: string;
+  public description: string;
+  public averageGrade: number;
+
+  constructor(private pharmacyService: PharmacyService, private authService: AuthenticationService) {
+    this.pharmacyService.getPharmacyByPharmacyAdminId(this.authService.getLoggedUserId()).subscribe(
+      data => {
+        this.pharmacy = data;
+        this.prepareDate(this.pharmacy); 
+      }
+    )
+  }
+
+  prepareDate(pharmacy: Pharmacy) : void {
+    this.id = pharmacy.id;
+    this.name = pharmacy.name;
+    this.description = pharmacy.description;
+    this.address = pharmacy.country;
+  }
 
   ngOnInit(): void {
   }
---
+++ server/spring-boot-pharmacy/src/main/java/isa/spring/boot/pharmacy/repository/pharmacy/PharmacyRepository.java
@@ -0,0 +1,7 @@
+package isa.spring.boot.pharmacy.repository.pharmacy;
+
+import isa.spring.boot.pharmacy.model.pharmacy.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+}
