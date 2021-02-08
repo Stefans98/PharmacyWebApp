9f7aad6115b16c94858c527642103478fc855c0b
Stefan <stefan.savic98@gmail.com>
2021-02-09 00:39:34 +0100

    Designed homepage with required links for unauthenticated users

--- client/angular-pharmacy/src/app/app.module.ts
+++ client/angular-pharmacy/src/app/app.module.ts
@@ -109,6 +109,11 @@ import { PharmacyAdministratorProfileComponent } from './pharmacy-administrator/
 import { MedicineSpecificationModalDialogPharmacistComponent } from './pharmacist/pharmacist-start-appointment/medicine-specification-modal-dialog-pharmacist/medicine-specification-modal-dialog-pharmacist.component';
 import { SubscriptionMedicinesModalDialogPharmacistComponent } from './pharmacist/pharmacist-start-appointment/subscription-medicines-modal-dialog-pharmacist/subscription-medicines-modal-dialog-pharmacist.component';
 import { PharmacyChoosingComponent } from './patient/dermatologist/pharmacy-choosing/pharmacy-choosing.component';
+import { NonAuthenticatedUserToolbarComponent } from './core-non-auth/non-authenticated-user-toolbar/non-authenticated-user-toolbar.component';
+import { NonAuthenticatedUserMenuComponent } from './core-non-auth/non-authenticated-user-menu/non-authenticated-user-menu.component';
+import { NonAuthenticatedHomePageComponent } from './core-non-auth/non-authenticated-home-page/non-authenticated-home-page.component';
+import { NonAuthenticatedPharamcySearchComponent } from './core-non-auth/non-authenticated-pharamcy-search/non-authenticated-pharamcy-search.component';
+import { NonAuthenticatedMedicinesSearchComponent } from './core-non-auth/non-authenticated-medicines-search/non-authenticated-medicines-search.component';
 
 @NgModule({
   declarations: [
@@ -206,7 +211,12 @@ import { PharmacyChoosingComponent } from './patient/dermatologist/pharmacy-choo
     PenaltyGraphComponent,
     MedicineSpecificationModalDialogPharmacistComponent,
     SubscriptionMedicinesModalDialogPharmacistComponent,
-    PharmacyChoosingComponent
+    PharmacyChoosingComponent,
+    NonAuthenticatedUserToolbarComponent,
+    NonAuthenticatedUserMenuComponent,
+    NonAuthenticatedHomePageComponent,
+    NonAuthenticatedPharamcySearchComponent,
+    NonAuthenticatedMedicinesSearchComponent
   ],
   imports: [
     BrowserModule,
--- client/angular-pharmacy/src/app/lazy-load/lazy-load.module.ts
+++ client/angular-pharmacy/src/app/lazy-load/lazy-load.module.ts
@@ -4,9 +4,10 @@ import { Routes,RouterModule } from '@angular/router';
 import { AuthModule } from '../auth/auth.module';
 import { HomeComponent } from '../home/home.component';
 import { LoginComponent } from '../login/login.component'
+import { AllPharmaciesComponent } from '../patient/pharmacy/all-pharmacies/all-pharmacies.component';
 
 const routes: Routes = [   
-    {path: 'home', component: HomeComponent},
+    {path: 'home', component: HomeComponent, children : [{path: 'pharmacy/all-pharmacies', component: AllPharmaciesComponent}]},
     {path: 'login', component: LoginComponent},
     {path: 'auth', loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule)},
     {path: '**', redirectTo: 'home'},
